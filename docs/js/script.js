773c6b5181d727d5bf697ab4c83cee52402d4248
tsjuus <guusmay2@hotmail.com>
2021-02-05 16:44:25 +0100

    added backend for temps and logged average temp

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,4 +1,5 @@
-const url = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-1-31&api_key=i8b5WtpKTdfadlaSh7So7ZZmn7T7q2UfDSaS3ABs'
+const photosUrl = 'https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2021-1-31&api_key=i8b5WtpKTdfadlaSh7So7ZZmn7T7q2UfDSaS3ABs'
+const tempsUrl = 'https://wafs-be.herokuapp.com/temps'
 
 const body = document.querySelector('body')
 
@@ -16,24 +17,30 @@ function randomPhoto(photos) {
 }
 
 
-function averageTemp(mydata) {
+function averageTemperature(data) {
   data.forEach(obj => {
     averageTemps.push(obj.temps.average)
   });
+
+  for (let i = 0; i < averageTemps.length; i++) {
+    temps += averageTemps[i]
+  }
+
+  const averageTemp = temps / averageTemps.length
+  
+  console.log(averageTemp.toFixed(2))
 }
 
-averageTemp(data)
+// console.log(temps / averageTemps.length)
 
-for (let i = 0; i < averageTemps.length; i++) {
-  temps += averageTemps[i]
-}
-
-console.log(temps / averageTemps.length)
-
-const apiData = fetch(url)
+const photoData = fetch(photosUrl)
   .then(response => response.json())
   .then(data => {
     console.log(data)
     randomPhoto(data.photos)
   }
   )
+
+const tempData = fetch(tempsUrl)
+  .then(res => res.json())
+  .then(d => averageTemperature(d))
