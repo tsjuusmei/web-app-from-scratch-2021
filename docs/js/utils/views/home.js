7bf0751d0312ac5a877c18b6b79af5bc73ca3d67
tsjuus <guusmay2@hotmail.com>
2021-02-19 14:47:07 +0100

    added third url and fetch for testing

--- docs/js/utils/views/home.js
+++ docs/js/utils/views/home.js
@@ -1,16 +1,19 @@
 import { averageTemperature, liData } from '../helpers/appends'
 import { fetchData } from '../helpers/fetchData'
-import { tempUrl, photoUrl, sol3035Url } from '../helpers/endpoints'
+import { tempUrl, photoUrl, sol3035Url, sol3034Url } from '../helpers/endpoints'
 
 export const homeView = () => {
   return async () => {
     const tempData = await fetchData(tempUrl)
     averageTemperature(tempData)
 
+    const sol3035Data = await fetchData(sol3035Url)
+    liData(sol3035Data)
+
+    const sol3034Data = await fetchData(sol3034Url)
+    liData(sol3034Data)
+
     const photoData = await fetchData(photoUrl)
     liData(photoData)
-
-    const solData = await fetchData(sol3035Url)
-    liData(solData)
   }
 }
