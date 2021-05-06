709f4b84b73870424af4e1b359464079c3a18286
tsjuusmei <guusmay2@hotmail.com>
2021-05-06 16:38:52 +0200

    updates

--- docs/js/utils/views/detail.js
+++ docs/js/utils/views/detail.js
@@ -1,14 +1,15 @@
 import { fetchData } from '../helpers/fetchData'
 import { detailInfo, detailPhotos } from '../helpers/appends'
+import { solUrl } from '../helpers/endpoints'
 
 export const detailView = () => {
   return async () => {
     let sol = window.location.href
     sol = sol.replace('http://localhost:1234/sol/', '')
 
-    const endpoint = "https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=" + sol + "&api_key=i8b5WtpKTdfadlaSh7So7ZZmn7T7q2UfDSaS3ABs&page=1"
+    const url = solUrl(sol)
     
-    const solData = await fetchData(endpoint)
+    const solData = await fetchData(url)
     let earthDate = solData.photos[0].earth_date
 
     detailInfo(sol, earthDate)
